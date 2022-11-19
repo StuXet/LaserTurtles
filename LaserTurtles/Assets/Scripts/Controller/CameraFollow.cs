bf3c0ac73db8f02ddb32481bdc0e8098e17dcd41
George Rashed <goerge1110@gmail.com>
2022-11-19 23:18:11 +0200

    Squashed commit of the following:
    commit e262569ee19a069b38e7c9a0701a25c544fb52aa
    Author: George Rashed <goerge1110@gmail.com>
    Date:   Sat Nov 19 23:07:49 2022 +0200
    
        Replaced Rigidbody with CharacterController, Improved Movement System with 3 Movement Options.
    
        Also added Gridboxed Materials Package.
    
    commit ffe57c7757c6f004a3a413af66d58ed44769b087
    Author: George Rashed <goerge1110@gmail.com>
    Date:   Sat Nov 19 20:03:31 2022 +0200
    
        Tweaked Movement & CameraFollow.
    
    commit fd20b785f3c5cb6613020859f95b5a4962b1c1b6
    Author: George Rashed <goerge1110@gmail.com>
    Date:   Sat Nov 19 17:57:02 2022 +0200
    
        Squashed commit of the following:
    
        commit 3a90e864f49bd37f5f100b236659a7becd49b60b
        Author: Mark <mark489@live.ru>
        Date:   Thu Nov 17 18:43:40 2022 +0200
    
            Basic Player Controller
    
            + X Z Axis movement
            + Looking direction by mouse or arrows
    
        commit 164051b5819fc06eaa7554cffea52959854fdf00
        Author: StuXet <36419721+StuXet@users.noreply.github.com>
        Date:   Tue Nov 15 18:56:39 2022 +0200
    
            Stuff for the player

---
+++ LaserTurtles/Assets/Scripts/Controller/CameraFollow.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // Variables
+    // --------------------
+    private Camera _thisCamera;
+    [SerializeField] private GameObject _objToFollow;
+    public float OffsetDistance = 10;
+    private Vector3 _camOffset;
+    public bool Follow;
+
+
+    // Default Methods
+    // --------------------
+    private void Awake()
+    {
+        _thisCamera = GetComponent<Camera>();    
+    }
+
+    void Update()
+    {
+        CalibrateDistance();
+    }
+
+    private void LateUpdate()
+    {
+        SettingFollowPos();
+    }
+
+
+    // Created Methods
+    // --------------------
+    private void CalibrateDistance()
+    {
+        _camOffset = new Vector3(-OffsetDistance, OffsetDistance, -OffsetDistance);
+        _thisCamera.orthographicSize = OffsetDistance;
+    }
+
+    private void SettingFollowPos()
+    {
+        if (Follow && _objToFollow)
+        {
+            transform.position = _camOffset + _objToFollow.transform.position;
+        }
+        else
+        {
+            transform.position = _camOffset;
+        }
+    }
+}
---
+++ LaserTurtles/Assets/Scripts/Controller/PlayerController.cs
@@ -0,0 +1,158 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using static UnityEngine.GraphicsBuffer;
+
+
+// Movement Type Enum
+// --------------------
+public enum MovementType
+{
+    WorldPos,
+    WorldPosTrackCursor,
+    TowardsCursor
+}
+
+
+public class PlayerController : MonoBehaviour
+{
+    // Variables
+    // --------------------
+    private Camera _playerCam;
+    private CharacterController _charCon;
+
+    public bool InControl = true;
+
+    [Header("Movement & Looking")]
+    public float Speed = 10.0f;
+    public MovementType MoveType = MovementType.WorldPos;
+    //public bool LookCursor = false;
+    //public bool WorldSpaceMove = true;
+    private Vector3 _movementDir;
+    private Vector3 _skewedMoveDir;
+
+    [Header("Gravity")]
+    public LayerMask GroundMask;
+    public Transform GroundCheck;
+    public float GroundDistance = 0.5f;
+    public float GravityModifier = 10f;
+    private Vector3 _velocity;
+    private bool _isGrounded;
+    public bool GravityEnabled = true;
+
+
+
+    // Default Methods
+    // --------------------
+    private void Awake()
+    {
+        _playerCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        _charCon = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        if (InControl)
+        {
+            MovementManager();
+        }
+        Gravity();
+    }
+
+
+    // Created Methods
+    // --------------------
+    private void MoveToWorldPos()
+    {
+        // Rotating Axis to Up
+        var matrixRot = Matrix4x4.Rotate(Quaternion.Euler(0, 45, 0));
+        _skewedMoveDir = matrixRot.MultiplyPoint3x4(_movementDir).normalized;
+
+
+        // Move, Normalise and make Vector proportional to the Speed per second.
+        _charCon.Move(_skewedMoveDir * Speed * Time.deltaTime);
+    }
+
+    private void RotateToCursor()
+    {
+        //player looks at the mouse LookCursor position
+        Ray ray = _playerCam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            Vector3 target = hit.point;
+            target.y = transform.position.y;
+            transform.LookAt(target);
+        }
+    }
+
+    void MovementManager()
+    {
+        // Get the horizontal and vertical input.
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        // Set the movement vector based on the axis input.
+        _movementDir.Set(h, 0f, v);
+
+
+        if (MoveType == MovementType.WorldPos) // Moves Player With World's Axis & Rotates Towards Move Direction
+        {
+            MoveToWorldPos();
+
+            //rotate the game object with the direction of the movement
+            if (_movementDir != Vector3.zero)
+            {
+                transform.rotation = Quaternion.LookRotation(_skewedMoveDir);
+            }
+        }
+        else if (MoveType == MovementType.WorldPosTrackCursor) // Moves Player With World's Axis & Rotates Towards Cursor Position
+        {
+            MoveToWorldPos();
+            RotateToCursor();
+        }
+        else if (MoveType == MovementType.TowardsCursor) // Rotates Player Towards Cursor Position & Moves Relative To It's Position (Follows Cursor)
+        {
+            RotateToCursor();
+
+            if (_movementDir.magnitude != 0)
+            {
+                // Move Player Towards Look Rotation
+                var matrixRot = Matrix4x4.Rotate(Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0));
+                _skewedMoveDir = matrixRot.MultiplyPoint3x4(_movementDir).normalized;
+                _charCon.Move(_skewedMoveDir * Speed * Time.deltaTime);
+            }
+        }
+    }
+
+
+    private void Gravity()
+    {
+        _isGrounded = Physics.CheckSphere(GroundCheck.position, GroundDistance, GroundMask);
+
+        if (GravityEnabled)
+        {
+
+            if (_isGrounded && _velocity.y < 0)
+            {
+                _velocity.y = -2;
+            }
+
+
+            _velocity.y -= GravityModifier * Time.deltaTime;
+            _charCon.Move(_velocity * Time.deltaTime);
+        }
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Color transparentGreen = new Color(0.0f, 1.0f, 0.0f, 0.35f);
+        Color transparentRed = new Color(1.0f, 0.0f, 0.0f, 0.35f);
+
+        if (_isGrounded) Gizmos.color = transparentGreen;
+        else Gizmos.color = transparentRed;
+
+        // when selected, draw a gizmo in the position of, and matching radius of, the grounded collider
+        Gizmos.DrawSphere(GroundCheck.position, GroundDistance);
+    }
+}
