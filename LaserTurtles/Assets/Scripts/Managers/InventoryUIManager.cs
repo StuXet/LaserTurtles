5d04b76e97c05fb934d747ac688b871f1f52f286
George Rashed <goerge1110@gmail.com>
2023-01-01 16:30:04 +0200

    Squashed commit of the following:
    commit 522263e48fa989002faa6af56f3392dc39aafd59
    Author: George Rashed <goerge1110@gmail.com>
    Date:   Sun Jan 1 16:29:34 2023 +0200
    
        Created Inventory Filter.
    
    commit da4d4afed724c95db361d1815cd7ab290604603d
    Author: George Rashed <goerge1110@gmail.com>
    Date:   Sat Dec 31 03:35:06 2022 +0200
    
        Inventory System Implemented with UI.
    
        Equipping Implementation is left.
    
    commit c56ced2ed2f0d3a3c9b4c9e6a968b43a334a476a
    Author: George Rashed <goerge1110@gmail.com>
    Date:   Sat Dec 31 00:06:06 2022 +0200
    
        Prepped Inventory System Basics.
    
    commit 36d07d24755c6b47cd014a12de0b432d72254068
    Author: George Rashed <goerge1110@gmail.com>
    Date:   Mon Dec 26 15:21:24 2022 +0200
    
        Started working on Inventory System.
    
    commit 0996134d9fea48081f0040b30ee7ce6e605db87b
    Author: George Rashed <goerge1110@gmail.com>
    Date:   Mon Dec 26 12:10:39 2022 +0200
    
        Squashed commit of the following:
    
        commit d33569fcdd624000ba13f6130387a88d93044dbe
        Author: shaya14 <84465665+shaya14@users.noreply.github.com>
        Date:   Sun Dec 25 18:21:22 2022 +0200
    
            Squashed commit of the following:
    
            commit 78315603a61be1d2173d0c9616b870da6de49e67
            Author: shaya14 <84465665+shaya14@users.noreply.github.com>
            Date:   Sun Dec 25 18:19:27 2022 +0200
    
                New Audio Folder

--- LaserTurtles/Assets/Scripts/Managers/InventoryUIManager.cs
+++ LaserTurtles/Assets/Scripts/Managers/InventoryUIManager.cs
@@ -3,11 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum InventoryFilter
+{
+    All,
+    Melee,
+    Ranged,
+}
+
 public class InventoryUIManager : MonoBehaviour
 {
     [SerializeField] private InventorySystem PlayerInventoryRef;
     [SerializeField] private GameObject _slotPrefab;
     [SerializeField] private Transform _contentBar;
+    [SerializeField] private InventoryFilter _currentFilter;
+
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +25,11 @@ public class InventoryUIManager : MonoBehaviour
     }
 
     private void OnUpdateInventory(object sender, System.EventArgs e)
+    {
+        RedrawInventory();
+    }
+
+    private void RedrawInventory()
     {
         foreach (Transform t in _contentBar)
         {
@@ -29,13 +43,35 @@ public class InventoryUIManager : MonoBehaviour
     {
         foreach (InventoryItem item in PlayerInventoryRef.InventoryItems)
         {
-            AddInventorySlot(item);
+            if (PassFilter(item))
+            {
+                AddInventorySlot(item);
+            }
         }
     }
 
     private void AddInventorySlot(InventoryItem item)
     {
-        GameObject obj = Instantiate(_slotPrefab,_contentBar,false);
+        GameObject obj = Instantiate(_slotPrefab, _contentBar, false);
         obj.GetComponent<InventorySlot>().Set(item);
+    }
+
+    private bool PassFilter(InventoryItem item)
+    {
+        if (_currentFilter == InventoryFilter.All)
+        {
+            return true;
+        }
+        else if (_currentFilter.ToString() == item.DataRef.Type.ToString())
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public void SetFilter(int filterVal)
+    {
+        _currentFilter = (InventoryFilter)filterVal;
+        RedrawInventory();
     }
 }
