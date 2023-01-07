eb103b1d9fade300c7ef0ac4b135b0cc7b57bd4f
George Rashed <goerge1110@gmail.com>
2023-01-07 18:38:03 +0200

    Added InteractionRequirement, Removes Item from Inv to Equip.

--- LaserTurtles/Assets/Scripts/Inventory/EquipmentSlot.cs
+++ LaserTurtles/Assets/Scripts/Inventory/EquipmentSlot.cs
@@ -6,15 +6,19 @@ using UnityEngine.EventSystems;
 public class EquipmentSlot : MonoBehaviour, IDropHandler
 {
     [SerializeField] private ItemType EquipType;
+    [SerializeField] private InventorySystem _inventorySystemRef;
+
     public void OnDrop(PointerEventData eventData)
     {
         if (transform.childCount == 0)
         {
             GameObject droppedObj = eventData.pointerDrag;
-            if (EquipType == droppedObj.GetComponent<InventorySlot>().ItemData.Type)
+            InventorySlot invSlot = droppedObj.GetComponent<InventorySlot>();
+            if (EquipType == invSlot.ItemData.Type)
             {
                 DraggableItem draggableItem = droppedObj.GetComponent<DraggableItem>();
                 draggableItem.OriginalParent = transform;
+                _inventorySystemRef.Remove(invSlot.ItemData);
                 Debug.Log("Dropped");
             }
         }
