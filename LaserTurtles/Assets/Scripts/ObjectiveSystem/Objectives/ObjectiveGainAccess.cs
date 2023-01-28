93610257158a66cd96b5528c13ea7dbdea880149
George Rashed <goerge1110@gmail.com>
2023-01-28 14:16:51 +0200

    Added Door Fade.

--- LaserTurtles/Assets/Scripts/ObjectiveSystem/Objectives/ObjectiveGainAccess.cs
+++ LaserTurtles/Assets/Scripts/ObjectiveSystem/Objectives/ObjectiveGainAccess.cs
@@ -5,12 +5,24 @@ using UnityEngine;
 
 public class ObjectiveGainAccess : ObjectiveBase
 {
+    [SerializeField] private bool _useFade = true;
     [SerializeField] private GameObject _door;
     [SerializeField] private GameObject _doorLockIcon;
     [SerializeField] private TextMeshPro _objectivesCounterText;
     [SerializeField] private List<ObjectiveBase> keyObjectives = new List<ObjectiveBase>();
 
+    private bool _openDoor;
+    private bool _doorOpened;
+    private Material _doorMat;
     private int _keyObjectivesCounter = 0;
+
+    private void Awake()
+    {
+        if (_door)
+        {
+            _doorMat = _door.GetComponent<MeshRenderer>().material;
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -35,13 +47,40 @@ public class ObjectiveGainAccess : ObjectiveBase
         FinishedObjective();
         CheckKeyStatus();
         RefreshText();
+        FadeDoor();
     }
 
 
     private void OpenDoor()
     {
-        _door.SetActive(false);
+        _openDoor = true;
         _objectivesCounterText.gameObject.SetActive(false);
+        if (!_useFade)
+        {
+            _door.SetActive(false);
+            _doorOpened = true;
+        }
+    }
+
+    private void FadeDoor()
+    {
+        if (!_doorOpened && _useFade)
+        {
+            if (_openDoor)
+            {
+                float fadeVal = _doorMat.GetFloat("_Fade_Depth");
+
+                fadeVal = Mathf.Lerp(fadeVal, 180, 2 * Time.deltaTime);
+
+                _doorMat.SetFloat("_Fade_Depth", fadeVal);
+            }
+
+            if (_doorMat.GetFloat("_Fade_Depth") >= 150)
+            {
+                _door.SetActive(false);
+                _doorOpened = true;
+            }
+        }
     }
 
     private void CheckKeyStatus()
