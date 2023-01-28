946be2d6efdd39391222355a3ac03692b7f05bb8
George Rashed <goerge1110@gmail.com>
2023-01-28 12:13:48 +0200

    Working on Door Objective

--- LaserTurtles/Assets/Scripts/ObjectiveSystem/Objectives/ObjectiveGainAccess.cs
+++ LaserTurtles/Assets/Scripts/ObjectiveSystem/Objectives/ObjectiveGainAccess.cs
@@ -1,17 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ObjectiveGainAccess : ObjectiveBase
 {
     [SerializeField] private GameObject _door;
-
+    [SerializeField] private GameObject _doorLockIcon;
+    [SerializeField] private TextMeshPro _objectivesCounterText;
     [SerializeField] private List<ObjectiveBase> keyObjectives = new List<ObjectiveBase>();
+
+    private int _keyObjectivesCounter = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         _door.SetActive(true);
+
+        if (keyObjectives.Count == 0)
+        {
+            _doorLockIcon.SetActive(false);
+            _objectivesCounterText.gameObject.SetActive(false);
+        }
+        else
+        {
+            _doorLockIcon.SetActive(true);
+            _objectivesCounterText.gameObject.SetActive(true);
+        }
     }
 
     // Update is called once per frame
@@ -19,28 +34,31 @@ public class ObjectiveGainAccess : ObjectiveBase
     {
         FinishedObjective();
         CheckKeyStatus();
-        OpenDoor();
+        RefreshText();
     }
 
 
     private void OpenDoor()
     {
-        if (CompletedObjective)
-        {
-            _door.SetActive(false);
-        }
+        _door.SetActive(false);
+        _objectivesCounterText.gameObject.SetActive(false);
     }
 
     private void CheckKeyStatus()
     {
         if (!CompletedObjective)
         {
+            _keyObjectivesCounter = 0;
             bool allDone = true;
             for (int i = 0; i < keyObjectives.Count; i++)
             {
                 if (!keyObjectives[i].CompletedObjective)
                 {
                     allDone = false;
+                }
+                else
+                {
+                    _keyObjectivesCounter++;
                 }
             }
 
@@ -51,11 +69,24 @@ public class ObjectiveGainAccess : ObjectiveBase
         }
     }
 
+    private void RefreshText()
+    {
+        if (CompletedObjective)
+        {
+            _doorLockIcon.SetActive(false);
+        }
+        _objectivesCounterText.text = _keyObjectivesCounter.ToString() + "/" + keyObjectives.Count.ToString();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (!CompletedObjective)
         {
             BeginObjective();
+        }
+        else
+        {
+            OpenDoor();
         }
     }
 }
