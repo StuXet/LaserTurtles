81e1915c7bc322e699ca512d20676a216aec0dbd
George Rashed <goerge1110@gmail.com>
2023-10-15 20:14:10 +0300

    Update PlayerUIManager.cs

--- LaserTurtles/Assets/Scripts/Managers/PlayerUIManager.cs
+++ LaserTurtles/Assets/Scripts/Managers/PlayerUIManager.cs
@@ -18,9 +18,6 @@ public class PlayerUIManager : MonoBehaviour
     private GameObject _objectiveUIRef;
     private Animator _objUIAnimator;
 
-    private GameObject _objectiveUIRef;
-    private Animator _objUIAnimator;
-
     // Start is called before the first frame update
     void Start()
     {
